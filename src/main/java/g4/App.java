ad28d706d237ae4b2422946980248f78112a7e21
Santiago Ocamica <santi6982@gmail.com>
2017-06-21 19:41:18 -0300

    Display Hitchcock image in main

--- src/main/java/g4/App.java
+++ src/main/java/g4/App.java
@@ -1,7 +1,13 @@
 package g4;
 
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
 public class App {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     System.out.println("Hola mundo");
+    BufferedImage image = ImageIO.read(App.class.getResource("/sin_secreto/Alfred.bmp"));
+    Util.displayImage(image);
   }
 }
